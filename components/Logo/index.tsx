43ca7700d1f25a8734fa97c15d8a374a235190d0
Your Name <>
2023-04-26 22:14:16 +0200

    fixed bug with link

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image"
 import Link from "next/link"
 import LogoIcon from 'public/img/logo.svg'
 
 const Logo = () => {
+
   return (
-    <Link className="logo-wrap" href="/">
+    <a className="logo-wrap" href={`/`}>
       <LogoIcon />
-    </Link>
+    </a>
   )
 }
 
