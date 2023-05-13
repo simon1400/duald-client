5d7b7c2b4c0a6a391df8172ff9647546016510f7
Your Name <>
2023-05-13 15:57:11 +0200

    add fixed category menu

--- layout/CategoryNav/index.tsx
+++ layout/CategoryNav/index.tsx
@@ -1,16 +1,13 @@
 import { Box, Container, Tab, Tabs } from "@mui/material";
-import { CategoryNavS } from "./styled";
-import { useState } from "react";
+import { CategoryNavS, ContainerWrap } from "./styled";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeActiveCategory, selectCategory } from "stores/slices/categoryNav";
 import { useLazyQuery } from "@apollo/client";
 import { getProducts } from "queries/products";
 import { useRouter } from "next/router";
 import { changeProducts } from "stores/slices/products";
-// import Image from "next/image";
-// import dynamic from "next/dynamic";
-
-// const APP_API = process.env.APP_API;
+import { useScroll } from "react-spring";
 
 const CategoryNav = () => {
   const [value, setValue] = useState(0);
@@ -18,9 +15,10 @@ const CategoryNav = () => {
   const categoryNav = useSelector(selectCategory);
 
   const router = useRouter()
-
+  const ref = useRef<HTMLDivElement>(null!);
+  const [scrollYState, setScrollYState] = useState<number>(0);
   const [updateProducts] = useLazyQuery(getProducts);
-
+  const [sticky, setSticky] = useState(false)
   const dispatch = useDispatch()
 
   const handleChange = async (event: React.SyntheticEvent, newValue: number) => {
@@ -28,26 +26,54 @@ const CategoryNav = () => {
     dispatch(changeActiveCategory(newValue))
     const newProducts = await updateProducts({variables: {category: categoryNav[newValue].slug, locale: router.locale}})
     dispatch(changeProducts(newProducts.data.products.data.map((item: any) => ({id: item.id, ...item.attributes}))))
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
 
+  const posElTop = useMemo(
+    () => ref?.current?.offsetTop,
+    [ref.current?.offsetTop]
+  );
+
+  useScroll({
+    onChange: ({ value: { scrollY } }) => setScrollYState(scrollY)
+  });
+
+  useEffect(() => {
+    if(posElTop) {
+      if (!sticky && scrollYState >= posElTop) {
+        setSticky(true)
+      }else if(sticky && scrollYState <= posElTop){
+        setSticky(false)
+      }
+    }
+  }, [scrollYState])
+  
   return (
-    <Container>
-      <CategoryNavS>
-        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            variant="scrollable"
-            scrollButtons={false}
-            aria-label="scrollable prevent tabs example"
-          >
-            {categoryNav.map((item: any, idx: number) => (
-              <Tab key={idx} label={item.title} />
-            ))}
-          </Tabs>
-        </Box>
-      </CategoryNavS>
-    </Container>
+    <section style={{height: "49px"}} ref={ref}>
+      <ContainerWrap sticky={sticky}>
+        <Container>
+          <CategoryNavS>
+            <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                allowScrollButtonsMobile
+                aria-label="scrollable prevent tabs example"
+              >
+                {categoryNav.map((item: any, idx: number) => (
+                  <Tab key={idx} label={item.title} />
+                ))}
+              </Tabs>
+            </Box>
+          </CategoryNavS>
+        </Container>
+      </ContainerWrap>
+    </section>
   );
 };
 
--- layout/CategoryNav/styled.ts
+++ layout/CategoryNav/styled.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Container } from "@mui/material";
 
 export const CategoryNavS = styled.section`
   border-bottom: 1px solid rgb(243, 245, 249);
@@ -7,3 +8,14 @@ export const CategoryNavS = styled.section`
     text-transform: none;
   }
 `
+
+export const ContainerWrap = styled.div<{sticky: boolean}>(({sticky = false}) => `
+  z-index: 1000;
+  position: ${sticky ? "fixed" : "static"}; 
+  top: 0; 
+  width: 100%; 
+  height: 49px; 
+  left: 0;
+  background: white;
+  box-shadow: ${sticky ? "0px 8px 45px rgba(3, 0, 71, 0.09)" : "none"};
+`)
